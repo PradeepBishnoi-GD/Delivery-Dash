35db65c41fef256b2b1f8eb17e2de1b297a1af60
PradeepBishnoi-GD <bipradeep888@gmail.com>
2025-12-25 16:07:36 +0530

    adding car capsule

---
+++ Delivery Dash/Assets/Scripts/Driver.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField]float steerSpeed = .5f;
+    [SerializeField]float moveSpeed = .05f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,steerSpeed);
+        transform.Translate(0,moveSpeed,0);
+    }
+}
